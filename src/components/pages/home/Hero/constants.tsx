7ef8992c2441ebbd49df86cb02828cbe8e1215e1
John Ossai <johnossai20@gmail.com>
2024-11-11 17:44:50 +0100

    Initial commit from Create Next App

---
+++ src/components/pages/home/Hero/constants.tsx
@@ -0,0 +1,19 @@
+
+
+export const carousel = [
+   {
+      imageUrl: "/image1.jpg",
+      title: "Ordination Tracks",
+      description:
+         "The Diploma in Ministry program is designed for men and women who are serving in a leadership role in their local church or another ministry position. The diploma provides a depth of training in the Bible, theology, and a broad range of ministry skills.",
+      ctaText: "Read More",
+      ctaUrl: "/products",
+   },
+   {
+      imageUrl: "/image2.jpg",
+      title: "Welcome to Our Company",
+      description: "Discover who we are and what we do.",
+      ctaText: "About Us",
+      ctaUrl: "/about",
+   },
+];
