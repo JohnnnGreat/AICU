efd0494d2ba8d049fd2057d37291872e841ee799
JohnnnGreat <102673699+JohnnnGreat@users.noreply.github.com>
2024-11-20 14:49:36 +0100

    Update index.tsx

--- src/components/Pages/Home/Hero/index.tsx
+++ src/components/Pages/Home/Hero/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Button } from "@/Components/ui/button";
+import { Button } from "@/components/ui/button";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/Components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Link from "next/link";
