a1e5d244cabd083e0b7af907149b75806106247c
John Ossai <johnossai20@gmail.com>
2024-12-04 16:59:53 +0100

    Fix Meta Issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Shared/Header";
 import Footer from "@/components/Shared/Footer";
+import Head from "next/head";
 
 export default function RootLayout({
    children,
@@ -21,6 +22,56 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body>
+            <title>American International Christian University | AICU</title>
+            <meta
+               name="description"
+               content="Welcome to American International Christian University (AICU), a leading Christian institution dedicated to providing quality education and transformational leadership. Explore our unique programs, discover our mission, and join us as we grow together."
+            />
+            {/* Open Graph Metadata */}
+            <meta
+               property="og:title"
+               content="American International Christian University | AICU"
+            />
+            <meta
+               property="og:description"
+               content="Welcome to AICU, a leading Christian institution dedicated to quality education and transformational leadership."
+            />
+            <meta
+               property="og:image"
+               content="/logo-uni.jpg"
+            />
+            <meta
+               property="og:url"
+               content="https://aicuedu.org"
+            />
+            <meta
+               property="og:type"
+               content="website"
+            />
+
+            {/* Twitter Card Metadata */}
+            <meta
+               name="twitter:card"
+               content="summary_large_image"
+            />
+            <meta
+               name="twitter:title"
+               content="American International Christian University | AICU"
+            />
+            <meta
+               name="twitter:description"
+               content="Welcome to AICU, a leading Christian institution dedicated to quality education and transformational leadership."
+            />
+            <meta
+               name="twitter:image"
+               content="/logo-uni.jpg"
+            />
+
+            {/* Favicon */}
+            <link
+               rel="icon"
+               href="/logo-uni.jpg"
+            />
             {isLoaded && (
                <div>
                   <Header />
--- src/components/Pages/Home/index.tsx
+++ src/components/Pages/Home/index.tsx
@@ -8,57 +8,6 @@ import AdvisorSection from "./AdvisorSection";
 const HomePage = () => {
    return (
       <>
-         <title>American International Christian University | AICU</title>
-         <meta
-            name="description"
-            content="Welcome to American International Christian University (AICU), a leading Christian institution dedicated to providing quality education and transformational leadership. Explore our unique programs, discover our mission, and join us as we grow together."
-         />
-         {/* Open Graph Metadata */}
-         <meta
-            property="og:title"
-            content="American International Christian University | AICU"
-         />
-         <meta
-            property="og:description"
-            content="Welcome to AICU, a leading Christian institution dedicated to quality education and transformational leadership."
-         />
-         <meta
-            property="og:image"
-            content="/logo-uni.jpg"
-         />
-         <meta
-            property="og:url"
-            content="https://aicuedu.org"
-         />
-         <meta
-            property="og:type"
-            content="website"
-         />
-
-         {/* Twitter Card Metadata */}
-         <meta
-            name="twitter:card"
-            content="summary_large_image"
-         />
-         <meta
-            name="twitter:title"
-            content="American International Christian University | AICU"
-         />
-         <meta
-            name="twitter:description"
-            content="Welcome to AICU, a leading Christian institution dedicated to quality education and transformational leadership."
-         />
-         <meta
-            name="twitter:image"
-            content="/logo-uni.jpg"
-         />
-
-         {/* Favicon */}
-         <link
-            rel="icon"
-            href="/logo-uni.jpg"
-         />
-
          <HeroSection />
          <Discover />
          <Programs />
