920035a2ae1a921a51b3273cfe0fe31c7d8f6104
John Ossai <johnossai20@gmail.com>
2024-12-13 20:33:23 +0100

    `Added next-sitemap dependency and updated package.json and package-lock.json accordingly`

---
+++ next-sitemap.js
@@ -0,0 +1,8 @@
+module.exports = {
+   siteUrl: "https://www.aicuedu.org",
+   generateRobotsTxt: true,
+   sitemapSize: 7000,
+   outDir: "./public",
+   changefreq: "daily",
+   priority: 0.7,
+};
